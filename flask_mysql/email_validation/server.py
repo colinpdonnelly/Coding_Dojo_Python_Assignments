814374cdc0cd4e0b8be1790607745578812bfbfc
Noel Danh <noeldanh@gmail.com>
2017-05-17 16:37:49 -0700

    email_validation

---
+++ flask_mysql/email_validation/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+mysql = MySQLConnector(app, 'full_friend')
+
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+
+app.run(debug=True)
