b9dc40dc037e155f1fc50cea606baf642651c60c
Noel Danh <noeldan1@yahoo.com>
2017-05-23 15:39:50 -0700

    update files

---
+++ django_assignments/wall/apps/my_wall/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class MyWallConfig(AppConfig):
+    name = 'my_wall'
---
+++ django_assignments/wall/apps/my_wall/models.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Messages(models.Model):
+    user_id = models.ForeignKey(Users)
+    message = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comments(models.Model):
+    user_id = models.ForeignKey(Users)
+    message_id = models.ForeignKey(Messages)
+    comment = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
